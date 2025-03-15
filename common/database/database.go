2b1c85487b834c740f6973cdffad1012f5798c68
Jordi Izquierdo <jordi.izquierdo.casares@gmail.com>
2025-03-15 20:11:32 +0100

    added custom indexes

--- common/database/database.go
+++ common/database/database.go
@@ -9,6 +9,13 @@ import (
 	"github.com/izquiratops/tango/common/types"
 
 	"github.com/blevesearch/bleve/v2"
+	"github.com/blevesearch/bleve/v2/analysis/analyzer/custom"
+	"github.com/blevesearch/bleve/v2/analysis/analyzer/keyword"
+	"github.com/blevesearch/bleve/v2/analysis/lang/cjk"
+	"github.com/blevesearch/bleve/v2/analysis/lang/en"
+	"github.com/blevesearch/bleve/v2/analysis/token/lowercase"
+	"github.com/blevesearch/bleve/v2/analysis/token/ngram"
+	"github.com/blevesearch/bleve/v2/analysis/tokenizer/unicode"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -56,25 +63,67 @@ func setupMongoDB(mongoURI string, collectionName string) (*mongo.Database, erro
 
 func setupBleve(dbVersion string) (bleve.Index, error) {
 	indexMapping := bleve.NewIndexMapping()
+
+	if err := indexMapping.AddCustomAnalyzer("english_no_stop", map[string]interface{}{
+		"type":      custom.Name,
+		"tokenizer": unicode.Name,
+		"token_filters": []string{
+			lowercase.Name,
+			en.PossessiveName,
+			en.StopName,
+		},
+	}); err != nil {
+		return nil, err
+	}
+
+	if err := indexMapping.AddCustomAnalyzer("japanese_ngram", map[string]interface{}{
+		"type":      custom.Name,
+		"tokenizer": unicode.Name,
+		"token_filters": []string{
+			lowercase.Name,
+			cjk.WidthName,
+			ngram.Name,
+		},
+		// https://github.com/blevesearch/bleve/blob/master/analysis/token/ngram/ngram.go
+		"token_maps": map[string]interface{}{
+			ngram.Name: map[string]interface{}{
+				"min": 2,
+				"max": 3,
+			},
+		},
+	}); err != nil {
+		return nil, err
+	}
+
 	documentMapping := bleve.NewDocumentMapping()
 
-	// "keyword" will treat the entire field as a single token
-	kanaExactMapping := bleve.NewTextFieldMapping()
-	kanaExactMapping.Analyzer = "keyword"
-	kanjiExactMapping := bleve.NewTextFieldMapping()
-	kanjiExactMapping.Analyzer = "keyword"
-
-	// Default mappings
-	kanaCharMapping := bleve.NewTextFieldMapping()
-	kanjiCharMapping := bleve.NewTextFieldMapping()
+	// English indexes
 	meaningsMapping := bleve.NewTextFieldMapping()
-
-	documentMapping.AddFieldMappingsAt("kana_exact", kanaExactMapping)
-	documentMapping.AddFieldMappingsAt("kana_char", kanaCharMapping)
-	documentMapping.AddFieldMappingsAt("kanji_exact", kanjiExactMapping)
-	documentMapping.AddFieldMappingsAt("kanji_char", kanjiCharMapping)
+	meaningsMapping.Analyzer = "english_no_stop"
 	documentMapping.AddFieldMappingsAt("meanings", meaningsMapping)
 
+	// Kana indexes
+	kanaExactMapping := bleve.NewTextFieldMapping()
+	kanaExactMapping.Analyzer = keyword.Name
+	documentMapping.AddFieldMappingsAt("kana_exact", kanaExactMapping)
+
+	kanaCharMapping := bleve.NewTextFieldMapping()
+	kanaExactMapping.Analyzer = "japanese_ngram"
+	documentMapping.AddFieldMappingsAt("kana_char", kanaCharMapping)
+
+	// Kanji indexes
+	kanjiExactMapping := bleve.NewTextFieldMapping()
+	kanjiExactMapping.Analyzer = keyword.Name
+	documentMapping.AddFieldMappingsAt("kanji_exact", kanjiExactMapping)
+
+	kanjiCharMapping := bleve.NewTextFieldMapping()
+	kanaExactMapping.Analyzer = "japanese_ngram"
+	documentMapping.AddFieldMappingsAt("kanji_char", kanjiCharMapping)
+
+	// TODO: Romaji!
+
+	// Default mapping
+	indexMapping.DefaultAnalyzer = "english_no_stop"
 	indexMapping.AddDocumentMapping("_default", documentMapping)
 
 	bleveFilename := fmt.Sprintf("jmdict_%v.bleve", dbVersion)
