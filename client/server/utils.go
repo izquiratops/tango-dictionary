c12ec8266f5fb360c8171e226b705d42d451babc
Jordi Izquierdo <jordi.izquierdo.casares@gmail.com>
2025-03-17 20:32:07 +0100

    add request logger

--- client/server/utils.go
+++ client/server/utils.go
@@ -1,6 +1,28 @@
 package server
 
-import "strings"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func (s *Server) logRequest(r *http.Request, statusCode int, duration time.Duration) {
+	ip := r.RemoteAddr
+	query := r.URL.Query().Get("query")
+
+	// Format: [TIMESTAMP] METHOD PATH STATUS IP DURATION QUERY
+	logMsg := fmt.Sprintf("[%s] %s %s %d %s %v %s",
+		time.Now().Format("2006-01-02 15:04:05"),
+		r.Method,
+		r.URL.Path,
+		statusCode,
+		ip,
+		duration,
+		query)
+
+	fmt.Println(logMsg)
+}
 
 func getContentType(path string) string {
 	switch {
